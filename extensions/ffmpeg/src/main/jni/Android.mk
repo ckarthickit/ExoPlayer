c66db6d6ff111eb4b9259c5cc4066c03f99853da
CHINNATHAMBI, KARTHICK <kc3503@intl.att.com>
2018-06-22 15:09:47 +0530

    Taking latest from feature/exo-with-ffmpeg

--- extensions/ffmpeg/src/main/jni/Android.mk
+++ extensions/ffmpeg/src/main/jni/Android.mk
@@ -34,7 +34,8 @@ include $(PREBUILT_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := ffmpeg
 LOCAL_SRC_FILES := ffmpeg_jni.cc
-LOCAL_C_INCLUDES := ffmpeg
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/ffmpeg
+LOCAL_CPP_INCLUDES := $(LOCAL_PATH)/ffmpeg/
 LOCAL_SHARED_LIBRARIES := libavcodec libavresample libavutil
 LOCAL_LDLIBS := -Lffmpeg/android-libs/$(TARGET_ARCH_ABI) -llog
 include $(BUILD_SHARED_LIBRARY)
